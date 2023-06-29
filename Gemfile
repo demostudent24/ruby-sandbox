6b8dd80d0d8b1d2f3eb48d0dcb82b504a202509c
demostudent24 <136644278+demostudent24@users.noreply.github.com>
2023-06-29 00:28:50 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "activesupport", "~> 3.2.1"
+gem "awesome_print"
+gem "pry-byebug"
+gem "sinatra"
---
+++ howdy.rb
@@ -0,0 +1,11 @@
+require "active_support/all"
+require "./goodbye.rb"
+require "awesome_print"
+
+# my_string = "Howdy there!"
+
+# pp my_string
+
+x = 3
+
+pp x.ordinalize
